2596f693d35b922117ef6cd49823be2570681ca5
Michael594tech <michaeltonga30@gmail.com>
2023-11-07 15:48:58 -0800

    breathe

---
+++ strlen.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+ * our_strlen - Calculate the length of a string
+ * @str: The input string
+ *
+ * Description:
+ * This function counts the number of characters in the string 'str' until
+ * it encounters a null-terminating character ('\0').
+ *
+ * Return:
+ * The number of characters in the string.
+ */
+size_t our_strlen(const char *str)
+{
+	size_t i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
